54894ef9284af28037be6d789f4317c588635a61
Alexandre Pliarchopoulos <al.pliar@gmail.com>
2022-06-17 10:08:04 +0200

    refactor: use arrow functions instead of es5 functions

--- challenges/exercise008-bonus-activity.js
+++ challenges/exercise008-bonus-activity.js
@@ -1,37 +1,37 @@
 // Spread Syntax Bonus activity
 
-function printColours(colours) {
-	if (!colours) throw new Error('colour is required');
-	// Your solution using the spread operator here!
-}
+const printColours = (colours) => {
+  if (!colours) throw new Error("colour is required");
+  // Your solution using the spread operator here!
+};
 
-function shoppingList(list, newItem) {
-	if (!list) throw new Error('list is required');
-	if (!newItem) throw new Error('newItem is required');
-	// Your solution using the spread operator here!
-}
+const shoppingList = (list, newItem) => {
+  if (!list) throw new Error("list is required");
+  if (!newItem) throw new Error("newItem is required");
+  // Your solution using the spread operator here!
+};
 
-function highestNumber(numbers) {
-	if (!numbers) throw new Error('numbers are required');
-	// Your solution using the spread operator here!
-}
+const highestNumber = (numbers) => {
+  if (!numbers) throw new Error("numbers are required");
+  // Your solution using the spread operator here!
+};
 
-function splitThatString(string) {
-	if (!string) throw new Error('string is required');
-	// Your solution using the spread operator here!
-}
+const splitThatString = (string) => {
+  if (!string) throw new Error("string is required");
+  // Your solution using the spread operator here!
+};
 
 // Optional Chaining Bonus activity!
 
-function addressLookUp(user) {
-	if (!user) throw new Error('user is required');
-	// Your solution using optional chaining here!
-}
+const addressLookUp = (user) => {
+  if (!user) throw new Error("user is required");
+  // Your solution using optional chaining here!
+};
 
 module.exports = {
-	printColours,
-	shoppingList,
-	highestNumber,
-	splitThatString,
-	addressLookUp,
+  printColours,
+  shoppingList,
+  highestNumber,
+  splitThatString,
+  addressLookUp,
 };
