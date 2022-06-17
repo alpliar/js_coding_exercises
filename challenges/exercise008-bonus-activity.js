97f4e2d754c7b28339e6f9eb14e5817ca155f91e
Alexandre Pliarchopoulos <al.pliar@gmail.com>
2022-06-17 17:11:06 +0200

    feature: write exercise 08

--- challenges/exercise008-bonus-activity.js
+++ challenges/exercise008-bonus-activity.js
@@ -3,22 +3,26 @@
 const printColours = (colours) => {
   if (!colours) throw new Error("colour is required");
   // Your solution using the spread operator here!
+  return [...colours];
 };
 
 const shoppingList = (list, newItem) => {
   if (!list) throw new Error("list is required");
   if (!newItem) throw new Error("newItem is required");
   // Your solution using the spread operator here!
+  return [...list, ...newItem];
 };
 
 const highestNumber = (numbers) => {
   if (!numbers) throw new Error("numbers are required");
   // Your solution using the spread operator here!
+  return numbers.find((num) => num === Math.max(...numbers));
 };
 
 const splitThatString = (string) => {
   if (!string) throw new Error("string is required");
   // Your solution using the spread operator here!
+  return string.split("");
 };
 
 // Optional Chaining Bonus activity!
@@ -26,6 +30,8 @@ const splitThatString = (string) => {
 const addressLookUp = (user) => {
   if (!user) throw new Error("user is required");
   // Your solution using optional chaining here!
+  if (user.address) return user.address.postcode;
+  return undefined;
 };
 
 module.exports = {
