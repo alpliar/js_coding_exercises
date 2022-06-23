2dcc4ec5ec3a1671a7ec83203f113e9ad846b072
Alexandre Pliarchopoulos <al.pliar@gmail.com>
2022-06-23 09:56:06 +0200

    feature: implement exercise003

--- challenges/exercise003.js
+++ challenges/exercise003.js
@@ -1,3 +1,5 @@
+const { capitalize } = require("./exercise001");
+
 /**
  * Get square of given numbers
  * @param {Array<number>} nums - List of numbers
@@ -6,6 +8,7 @@
 const getSquares = (nums) => {
   if (nums === undefined) throw new Error("nums is required");
   // Your code here!
+  return nums.map((num) => Math.pow(num, 2));
 };
 
 /**
@@ -16,6 +19,11 @@ const getSquares = (nums) => {
 const camelCaseWords = (words) => {
   if (words === undefined) throw new Error("words is required");
   // Your code here!
+  return words.reduce((acc, word, index) => {
+    const formattedWord = capitalize(word);
+
+    return acc + (index > 0 ? formattedWord : word);
+  }, "");
 };
 
 /**
@@ -26,6 +34,9 @@ const camelCaseWords = (words) => {
 const getTotalSubjects = (people) => {
   if (people === undefined) throw new Error("people is required");
   // Your code here!
+  return people.reduce((count, peep) => {
+    return count + peep.subjects.length;
+  }, 0);
 };
 
 /**
@@ -38,6 +49,7 @@ const checkIngredients = (menu, ingredient) => {
   if (menu === undefined) throw new Error("menu is required");
   if (!ingredient) throw new Error("ingredient is required");
   // Your code here!
+  return menu.some((meal) => meal.ingredients.includes(ingredient));
 };
 
 /**
@@ -50,6 +62,10 @@ const duplicateNumbers = (arr1, arr2) => {
   if (arr1 === undefined) throw new Error("arr1 is required");
   if (arr2 === undefined) throw new Error("arr2 is required");
   // Your code here!
+
+  return [
+    ...new Set(arr1.filter((num) => arr2.includes(num)).sort((a, b) => a - b)),
+  ];
 };
 
 module.exports = {
