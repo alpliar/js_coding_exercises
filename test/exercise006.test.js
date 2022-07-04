fa9d50a3ce8378b7e7a3ec17943b47f56e4470ae
Alexandre Pliarchopoulos <al.pliar@gmail.com>
2022-07-04 16:24:30 +0200

    tests: write test for exercise006 sumMultiples function

--- test/exercise006.test.js
+++ test/exercise006.test.js
@@ -1,10 +1,16 @@
 const { sumMultiples } = require("../challenges/exercise006");
 
 describe("sumMultiples", () => {
-  test("returns the next number after the given number in the array", () => {
+  test("returns 0 if there is no multiple of 3 or 5", () => {
+    expect(sumMultiples([0])).toBe(0);
     expect(sumMultiples([1])).toBe(0);
+    expect(sumMultiples([2, 8])).toBe(0);
+  });
+  test("returns the sum of a list of multiples of 3 and 5", () => {
     expect(sumMultiples([3])).toBe(3);
     expect(sumMultiples([3, 5])).toBe(8);
+  });
+  test("returns the sum of numbers which are multiples of 3 or 5", () => {
     expect(sumMultiples([1, 2, 3, 5, 8])).toBe(8);
   });
 });
