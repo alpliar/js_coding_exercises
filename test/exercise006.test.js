26a89b18e47ead59da068c4258d5e7fe3907d113
Alexandre Pliarchopoulos <al.pliar@gmail.com>
2022-07-04 16:37:37 +0200

    tests: write test for exercise006 getComplementaryDNA function

--- test/exercise006.test.js
+++ test/exercise006.test.js
@@ -1,4 +1,8 @@
-const { sumMultiples, isValidDNA } = require("../challenges/exercise006");
+const {
+  sumMultiples,
+  isValidDNA,
+  getComplementaryDNA,
+} = require("../challenges/exercise006");
 
 describe("sumMultiples", () => {
   test("returns 0 if there is no multiple of 3 or 5", () => {
@@ -27,5 +31,22 @@ describe("isValidDNA", () => {
   });
   test("should not be case sensitive", () => {
     expect(isValidDNA("gattaca")).toBe(true);
+  });
+});
+
+describe("getComplementaryDNA", () => {
+  test("T is paired with A", () => {
+    expect(getComplementaryDNA("T")).toBe("A");
+    expect(getComplementaryDNA("A")).toBe("T");
+  });
+  test("C is paired with G", () => {
+    expect(getComplementaryDNA("C")).toBe("G");
+    expect(getComplementaryDNA("G")).toBe("C");
+  });
+  test("gives correct complementary DNA", () => {
+    expect(getComplementaryDNA("ACTG")).toBe("TGAC");
+    expect(getComplementaryDNA("TGAC")).toBe("ACTG");
+    expect(getComplementaryDNA("AATT")).toBe("TTAA");
+    expect(getComplementaryDNA("GGTG")).toBe("CCAC");
   });
 });
