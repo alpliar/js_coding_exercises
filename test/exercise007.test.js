f1aa97c68010a2aea073d0726d25151583363809
Alexandre Pliarchopoulos <al.pliar@gmail.com>
2022-07-04 17:13:32 +0200

    tests: write test for exercise007 createRange function

--- test/exercise007.test.js
+++ test/exercise007.test.js
@@ -1,4 +1,4 @@
-const { sumDigits } = require("../challenges/exercise007");
+const { sumDigits, createRange } = require("../challenges/exercise007");
 
 describe("sumDigits", () => {
   test("returns given number if there is only one digit", () => {
@@ -11,5 +11,23 @@ describe("sumDigits", () => {
     expect(sumDigits(12341234)).toBe(20);
     expect(sumDigits(123123123123)).toBe(12);
     expect(sumDigits(1234567890)).toBe(45);
+  });
+});
+
+describe("createRange", () => {
+  test("returns correct range from 3 to 11 with step of 2", () => {
+    expect(createRange(3, 11, 2)).toBe([3, 5, 7, 9, 11]);
+  });
+  test("returns correct range from 0 to 11 with explicit step of 1", () => {
+    expect(createRange(0, 11, 1)).toBe([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]);
+  });
+  test("returns correct range from 0 to 11 without explicit step", () => {
+    expect(createRange(0, 11)).toBe([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]);
+  });
+  test("returns correct range from 0 to 100 with step of 25", () => {
+    expect(createRange(0, 100, 25)).toBe([0, 25, 50, 75, 100]);
+  });
+  test("returns correct range if start and end are equals", () => {
+    expect(createRange(0, 0, 25)).toBe([0]);
   });
 });
